2d01585197de4ed7097a5e21236be3e7ada01ffc
Maxime <maxime.gouillardon@gmail.com>
2024-09-02 21:26:51 +0200

    fix: rename login controller store method & create register controller

--- app/controllers/auth/register_controller.ts
+++ app/controllers/auth/register_controller.ts
@@ -1,14 +1,14 @@
 import User from '#models/user'
 import type { HttpContext } from '@adonisjs/core/http'
 
-export default class LoginController {
+export default class RegisterController {
   public async index({ view }: HttpContext) {
-    return view.render('pages/auth/login')
+    return view.render('pages/auth/register')
   }
 
   public async store({ auth, request, response }: HttpContext) {
     const { email, password } = request.only(['email', 'password'])
-    const user = await User.verifyCredentials(email, password)
+    const user = await User.create({ email, password })
 
     try {
       await auth.use('web').login(user)
