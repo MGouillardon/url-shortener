29f9f34cee1b9874c8e3edfcb0afcec8f000c36b
Maxime <maxime.gouillardon@gmail.com>
2024-09-03 00:07:32 +0200

    feat: add logout method to route + implement it in url view

--- start/routes.ts
+++ start/routes.ts
@@ -11,13 +11,27 @@ const LoginController = () => import('#controllers/auth/login_controller')
 const RegisterController = () => import('#controllers/auth/register_controller')
 const UrlController = () => import('#controllers/url_controller')
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 router.on('/').render('pages/auth/login')
 
-router.get('/auth/login', [LoginController, 'index']).as('auth.login')
-router.post('/auth/login', [LoginController, 'handle']).as('auth.login.handle')
+router
+  .group(() => {
+    router.get('/login', [LoginController, 'index']).as('auth.login')
+    router.post('/login', [LoginController, 'handle']).as('auth.login.handle')
+    router.get('/register', [RegisterController, 'index']).as('auth.register')
+    router.post('/register', [RegisterController, 'store']).as('auth.register.store')
+    router
+      .post('/logout', async ({ auth, response }) => {
+        await auth.use('web').logout()
+        return response.redirect().toRoute('auth.login')
+      })
+      .as('auth.logout')
+  })
+  .prefix('/auth')
 
-router.get('/auth/register', [RegisterController, 'index']).as('auth.register')
-router.post('/auth/register', [RegisterController, 'store']).as('auth.register.store')
-
-router.get('url', [UrlController, 'index']).as('url')
+router
+  .group(() => {
+    router.get('url', [UrlController, 'index']).as('url')
+  })
+  .use(middleware.auth())
