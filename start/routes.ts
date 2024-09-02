622a0924c3ed9c5bb879c0028627d3d56eaadb9e
Maxime <maxime.gouillardon@gmail.com>
2024-09-02 21:24:15 +0200

    feat(routes): make sign in form default route

--- start/routes.ts
+++ start/routes.ts
@@ -10,7 +10,7 @@
 const LoginController = () => import('#controllers/admin/login_controller')
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/auth/login')
 
 router.get('/auth/login', [LoginController, 'index']).as('auth.login')
 router.post('/auth/login', [LoginController, 'store']).as('auth.login.store')
